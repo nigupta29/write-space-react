ddd55e1f744378fd45a70911fe1b4fcdb4524a54
Nikhil Gupta <jerrycodes29@gmail.com>
2023-05-09 19:53:03 +0530

    add prettier plugin and not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Archive from "./pages/Archive"
-import BlogPost from "./pages/BlogPost"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import NotFound from "./pages/NotFound"
-import Register from "./pages/Register"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Archive from './pages/Archive'
+import BlogPost from './pages/BlogPost'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import NotFound from './pages/NotFound'
+import Register from './pages/Register'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/blog/:id' element={<BlogPost />} />
-        <Route path='/blog/archives' element={<Archive />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
+        <Route path="/blog/archives" element={<Archive />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 
 export default function NotFound() {
   return (
     <section>
-      <h1 className='text-5xl font-bold'>Not Found</h1>
-      <Link to={"/"}>Back to Home</Link>
+      <h1 className="text-5xl font-bold">Not Found</h1>
+      <Link to={'/'} className="flex w-fit items-center gap-2 bg-gray-100 p-3">
+        Back to Home <i className="fa-solid fa-arrow-right"></i>
+      </Link>
     </section>
   )
 }
